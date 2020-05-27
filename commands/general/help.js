83df5cb9348cf5971e463eeba1a6c92c83c5dec3
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-27 17:59:58 -0400

    Add files via upload

--- commands/general/help.js
+++ commands/general/help.js
@@ -3,9 +3,10 @@ const { MessageEmbed } = require("discord.js");
 module.exports.run = async (bot, message, args) => {      
       const helpembed = new MessageEmbed()
       .setTitle('Help List ')
-      .addField('Administrator','``\nban``, ``kick``, ``report``, ``poll``, ``purge, ``')
-      .addField('NSFW','```\nporn, ecchi, 4k, amateur, anal, ass, asshole, bbw, bdsm, bikinis, blonde, bottomless, brunette, collage, cosplay, curvy, dick, echhi, furrynsfw, gonewild, lewd, nsfw, milf, moreporn, neko, nude, onoff, petite, porn, porngif, public, pussy, redhead, redtube, rule34, snapchat, tattos, tits, uniform```')
+      .addField('Administrator','``\nban``, ``kick``, ``report``, ``poll``, ``purge``')
+      .addField('NSFW','``\nporn``, ``ecchi``, ``4k``, ``amateur``, ``anal``, ``ass``, ``asshole``, ``bbw``, ``bdsm``, ``bikinis``, ``blonde``, ``bottomless``, ``brunette``, ``collage``, ``cosplay``, ``curvy``, ``dick``, ``echhi``, ``furrynsfw``, ``gonewild``, ``lewd``, ``nsfw``, ``milf``, ``moreporn``, ``neko``, ``nude``, ``onoff``, ``petite``, ``porn``, ``porngif``, ``public``, ``pussy``, ``redhead``, ``redtube``, ``rule34``, ``snapchat``, ``tattos``, ``tits``, ``uniform``')
       .addField('Roleplay','``\nbite``,``crush``')
+      .addField('Miscellaneous ','``\nembed``')
       .setColor('#001AFF')
       .setTimestamp()
       .setFooter(`Requested by ${message.author.tag} | Help list`)
