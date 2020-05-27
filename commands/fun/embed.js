8d5f2537a4b739e7458de31afa4e587df01c168a
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-27 17:57:17 -0400

    Add files via upload

--- commands/fun/embed.js
+++ commands/fun/embed.js
@@ -1,1 +1,15 @@
+const { MessageEmbed } = require("discord.js");
 
+module.exports.run = (bot, message, args, funcs) => {
+  const text = args.join(` `).substr(0, 1000) || "No text provided.";
+  const embed = new MessageEmbed().setDescription(text).setColor("#FFFFFF");
+  message.channel.send(embed);
+};
+
+module.exports.config = {
+  name: "embed",
+  aliases: ["em"],
+  usage: "Put a message in an embed.",
+  commandCategory: "fun",
+  cooldownTime: '5'
+};
