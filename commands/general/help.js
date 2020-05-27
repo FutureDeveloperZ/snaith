c62e3a91fc8c1690f97868ee3f9b4bcb9ed869dc
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-27 16:29:14 -0400

    Yeet

--- commands/general/help.js
+++ commands/general/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
       .setTitle('Help List ')
       .addField('Administrator','```\nban, kick, report, poll, purge, ```')
       .addField('NSFW','```\nporn, ecchi, 4k, amateur, anal, ass, asshole, bbw, bdsm, bikinis, blonde, bottomless, brunette, collage, cosplay, curvy, dick, echhi, furrynsfw, gonewild, lewd, nsfw, milf, moreporn, neko, nude, onoff, petite, porn, porngif, public, pussy, redhead, redtube, rule34, snapchat, tattos, tits, uniform```')
-      .addField('Roleplay','```\nbite```,```crush```')
+      .addField('Roleplay','``\nbite``,``crush``')
       .setColor('#001AFF')
       .setTimestamp()
       .setFooter(`Requested by ${message.author.tag} | Help list`)
