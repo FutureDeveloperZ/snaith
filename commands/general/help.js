ba369934ee174c99acd65da679bbde848b8b62cb
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-26 21:51:57 -0400

    Add files via upload

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,1 +1,17 @@
+const { MessageEmbed } = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {      
+      const helpembed = new MessageEmbed()
+      .setTitle('Help List ')
+      .addField('Administrator','```\nban, kick, poll, purge, tempmute```')
+      .addField('NSFW','```\nporn, ecchi, 4k, amateur, anal, ass, asshole, bbw, bdsm, bikinis, blonde, bottomless, brunette, collage, cosplay, curvy, dick, echhi, furrynsfw, gonewild, lewd, nsfw, milf, moreporn, neko, nude, onoff, petite, porn, porngif, public, pussy, redhead, redtube, rule34, snapchat, tattos, tits, uniform```')
+      .setColor('#001AFF')
+      .setTimestamp()
+      .setFooter(`Requested by ${message.author.tag} | Help list`)
+      return message.channel.send(helpembed);
+
+}
+ 
+module.exports.help = {
+  name: "help"
+}
