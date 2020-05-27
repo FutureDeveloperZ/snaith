6fa04e5b6af51d284ef6a23bad733f41a9fcd8d0
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-26 21:18:32 -0400

    Add files via upload

---
+++ obama.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+const config = require(`./config.json`);
+const prefix = config.prefix;
+
+client.commands = new Discord.Collection();
+client.afk = new Map();
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunc = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, (...args) => eventFunc.run(client, ...args));
+  });
+});
+
+fs.readdir(`./commands`, (err, directories) => {
+  if (err) return console.error(error);
+  directories.forEach(directory => {
+    fs.readdir(`./commands/${directory}`, (e, files) => {
+      if (e) return console.error(e);
+      files.forEach(file => {
+        const eventFunc = require(`./commands/${directory}/${file}`);
+        let eventName = file.split(".")[0];
+        client.commands.set(eventName, eventFunc);
+      });
+    });
+  });
+});
+
+client.login('NDUxMTI5ODgxNzI0NDUyODY0.XsGsQA.cp2ehBHfj1VKzlFcuBnGuH8EPb8');
