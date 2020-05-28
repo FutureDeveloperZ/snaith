01d7da62076d66b4dd90d28a4da6026992d01305
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-28 11:14:03 -0400

    Forgot to add stopTyping

--- commands/fun/facepalm.js
+++ commands/fun/facepalm.js
@@ -23,6 +23,7 @@ module.exports.run = async (bot, message, args, funcs) => {
       message.channel.send(embed);
     } catch (e) {
       return message.channel.send(`***Oh no, an error occurred: \`${e.message}\`. Try again later!***`);
+      message.channel.stopTyping());
     }
   } catch (err) {
     console.log(err);
