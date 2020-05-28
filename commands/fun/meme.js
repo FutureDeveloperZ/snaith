2b00504cfff8bdf9e01db2ce4af7d65f5ce4f68c
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-28 10:07:42 -0400

    Add files via upload

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args, funcs) => {
       if (!allowed.length) return funcs.send(`Can't find any other images right now, try again later.`);
       const randomnumber = Math.floor(Math.random() * allowed.length);
       const embed = new MessageEmbed()
-        .setColor('#36393E')
+        .setColor('#60FF00')
         .setTitle(allowed[randomnumber].data.title)
         .setDescription("Posted by: " + allowed[randomnumber].data.author)
         .setImage(allowed[randomnumber].data.url)
