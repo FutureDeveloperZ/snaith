1803c24529706c6e368e832e5c22c29d257d942c
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-28 18:45:41 -0400

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -13,5 +13,5 @@ module.exports.run = async(client, member, args) => {
    .setDescription("Welcome " + "``" + username + "``" + " To The Official " + server + "Discord!")
    .setTimestamp()
    .setFooter("He Joined at")
-  member.guild.channels.get("・welcome-bye ").send(wcEmbed);
+  member.guild.channels.cache.get("・welcome-bye ").send(wcEmbed);
 };
