11c6d67ce52a6b2660d07b2fe077ed4970ec7ad5
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-05-29 15:13:53 -0400

    Add files via upload

---
+++ commands/fun/say.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports.run = async (client, message, args) => {
+
+ if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+};
+
+module.exports.help = {
+  name: 'say'
+
+};
