093c1410227d35f62f959ed403c4db4521733d3a
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2020-06-01 01:31:44 -0400

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,18 +1,44 @@
-let Discord = require("discord.js");
+const Discord = require("discord.js");
+const Canvas = require("canvas");
+
 
 module.exports.run = async(client, member, message, args, funcs) => {
-  //Alt checker (7 days old) - Ducky & Wolfkid :) (Hopefully 12/08/18)
+
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'logs');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./resources/canvas/wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
+});
 
 
-  //Update 12/08/18 - Ducky & WolfKid :)
-  
-  // let username = member.user.username;
-  // let server = member.guild.name;
-  // let wcEmbed = new Discord.MessageEmbed()
-  // .setAuthor("Welcome")
-//   .setDescription("Welcome " + "``" + username + "``" + " To The Official " + server + "Discord!")
- //  .setTimestamp()
-//   .setFooter("He Joined at")
-//  member.guild.channels.cache.find(c => c.name === `・welcome-bye`) 
-// return message.channel.cache.send(wcEmbed);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,17 +1,17 @@
 let Discord = require("discord.js");
 module.exports.run = async (client, member, message, args, funcs) => {
-    let welcomechannel = member.guild.channels.cache.find(c => c.name === `・welcome-bye`);
-    if (!welcomechannel) return member.guild.createChannel(c => c.name === "text");
-    let username = member.user.username;
-    let server = member.guild.name;
-    let players = member.guild.memberCount;
-    let gbEmbed = new Discord.MessageEmbed()
-        .setTitle("Good Bye")
-        .setDescription("Good Bye" + "``" + username + "``" + ". Hope to see u later")
-        .setColor("RANDOM")
-        .setTimestamp()
-        .setFooter("He left At")
+   // let welcomechannel = member.guild.channels.cache.find(c => c.name === `・welcome-bye`);
+ //   if (!welcomechannel) return member.guild.createChannel(c => c.name === "text");
+ //   let username = member.user.username;
+//    let server = member.guild.name;
+ //   let players = member.guild.memberCount;
+   // let gbEmbed = new Discord.MessageEmbed()
+    //    .setTitle("Good Bye")
+    //    .setDescription("Good Bye" + "``" + username + "``" + ". Hope to see u later")
+    //    .setColor("RANDOM")
+   //     .setTimestamp()
+    //    .setFooter("He left At")
 
-    member.guild.channels.cache.find(c => c.name === `・welcome-bye`);
-    return message.channel.cache.send(gbEmbed);
+   // member.guild.channels.cache.find(c => c.name === `・welcome-bye`);
+    // return message.channel.cache.send(gbEmbed);
 };
